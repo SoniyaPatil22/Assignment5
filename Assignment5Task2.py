fc8409d456bcbd13316a17a959ee5bc666f871b9
SoniyaPatil22 <sonia.dhole9622@gmail.com>
2025-07-22 17:20:16 +0530

    Add files via upload

---
+++ Assignment5Task2.py
@@ -0,0 +1,13 @@
+# 1. Create a list of numbers from 1 to 10
+numbers = list(range(1, 11))
+
+# 2. Extract the first five elements from the list
+first_five = numbers[:5]
+
+# 3. Reverse the extracted elements
+reversed_first_five = first_five[::-1]
+
+# 4. Print both the extracted list and the reversed list
+print("original list: ",numbers)
+print("Extracted First five elements:", first_five)
+print("Reversed extracted elements:", reversed_first_five)
